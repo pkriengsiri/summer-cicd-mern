d0bddb8d99b63b53ee940aeb7157f6be2b3185e7
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-06-22 19:27:30 -0400

    installed mocha and chai.  created restaurantController test

---
+++ test/controller/restaurantController.spec.js
@@ -0,0 +1,21 @@
+const mocha = require("mocha");
+const chai = require("chai");
+
+const expect = chai.expect;
+
+//describe
+describe("", () => {
+  //it block
+  it("", () => {});
+});
+
+//describe
+describe("restaurantController", () => {
+  describe("findById", () => {
+    //it block
+    it("should return a model if found", () => {});
+
+    it("should return an error if an error occurs", () => {});
+    
+  });
+});
