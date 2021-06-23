761203649663d0e6ae26e3a51d80efc3497422e9
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-06-22 20:18:52 -0400

    added unit tests for restaurant controller and updated develop yaml

--- test/controller/restaurantController.spec.js
+++ test/controller/restaurantController.spec.js
@@ -1,8 +1,12 @@
 const mocha = require("mocha");
 const chai = require("chai");
 const mongoose = require("mongoose");
+const sinon = require("sinon");
+const sinonChai = require("sinon-chai");
+const restaurantController = require("../../controllers/restaurantController");
 
 const expect = chai.expect;
+chai.use(sinonChai);
 
 //describe
 describe("", () => {
@@ -14,23 +18,64 @@ describe("", () => {
 describe("restaurantController", () => {
   describe("findById", () => {
     //it block
-    it("should return a model if found", () => {});
-    // Arrange
-    const req = {
+    it("should return a model if found", async () => {
+      // Arrange
+
+      const sandbox = sinon.createSandbox();
+
+      const req = {
         params: {
-            id: 1,
-        }
-    }
+          id: 1,
+        },
+      };
 
-    const res = {
-        json: () => {},
-    }
+      const res = {
+        json: sinon.spy(),
+      };
 
-    // create a mock
-    mongoose.Model.findById = 
-    // Act
-    // Assert
-    it("should return an error if an error occurs", () => {});
+      // create a mock
+      mongoose.Model.findById = sandbox
+        .stub()
+        .returns(Promise.resolve("resolved"));
 
+      // Act
+
+      await restaurantController.findById(req, res);
+
+      // Assert
+      // is res.json called and passed the string from the Promise.resolve above
+      expect(res.json).to.have.been.calledWith("resolved");
+    });
+
+    it("should return an error if an error occurs", async() => {
+      // Arrange
+      const sandbox = sinon.createSandbox();
+      const statusJsonSpy = sinon.spy();
+
+      const req = {
+        params: {
+          id: 1,
+        },
+      };
+
+      const res = {
+        json: sinon.spy(),
+        status: sinon.stub().returns({json: statusJsonSpy}),
+      };
+
+      mongoose.Model.findById = sandbox.stub().returns(Promise.reject("error"));
+
+      // Act
+
+      await restaurantController.findById(req,res);
+      await console.log("---");
+
+      // Assert
+
+      expect(res.status).to.have.been.calledWith(422);
+      expect(statusJsonSpy).to.have.been.calledWith("error");
+      
+
+    });
   });
 });
